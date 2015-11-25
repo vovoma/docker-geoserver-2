a94aed22fcb922c1d270c01affe283ea1be395f0
Brian Lee <blee@orionnetworksolutions.com>
2015-11-25 12:20:50 -0800

    Updated run.sh

--- run.sh
+++ run.sh
@@ -1,20 +1,25 @@
 #!/bin/env bash
 
-CONTAINER_HOME=$(cd $(dirname ${0}); pwd)
+function help {
+    echo "run.sh [options] [args]"
+    echo "    -h Display this help dialog"
+    echo "    -i Interactive mode, boots into shell of container (may conflict with args if supplied)"
+    echo "    -t Specifies tag for briandlee/geoserver image"
+    echo "    -v Mount volume, see docker documentation on \`docker run\`'s -v option"
+    exit ${1}
+}
+
+CONTAINER_HOME=$(cd $(dirname ${0}); pwd | sed 's/ /\\ /')
 
 mounts=("-v ${CONTAINER_HOME}/data:/var/lib/geoserver/data" "-v ${CONTAINER_HOME}/logs:/var/log/tomcat")
-
 options=('-d')
-
 tag='2.7'
-
 args=()
 
 while [[ ${1} ]]; do
     case ${1} in
-        -v)
-            shift
-            mounts+=("-v ${1}")
+        -h)
+            help 0
             ;;
         -i)
             options=('-it')
@@ -24,6 +29,10 @@ while [[ ${1} ]]; do
         -t)
             shift
             tag=${1}
+            ;;
+        -v)
+            shift
+            mounts+=("-v ${1// /\\ }")
             ;;
         -*)
             echo >2 'Unkown option provided: ${1}'
@@ -37,6 +46,6 @@ done
 
 pushd ${DOCKER_BUILD_LOCATION} 2>/dev/null
 
-docker run ${mounts[@]} ${options[@]} briandlee/geoserver@${tag} ${args}
+eval "docker run ${mounts[@]} ${options[@]} briandlee/geoserver@${tag} ${args}"
 
 popd 2>/dev/null
