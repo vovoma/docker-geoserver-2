af3b800762fb3f759793cfe858200ff13a9d183f
Brian Lee <brian.lee@orionnetworksolutions.com>
2016-02-03 20:31:04 -0800

    Updated to reflect the recent changes made in master and 2.8.x

--- run.sh
+++ run.sh
@@ -1,19 +1,26 @@
+
 #!/bin/env bash
 
 function help {
     echo "run.sh [options] [args]"
-    echo "    -h Display this help dialog"
-    echo "    -i Interactive mode, boots into shell of container (may conflict with args if supplied)"
-    echo "    -t Specifies tag for briandlee/geoserver image"
-    echo "    -v Mount volume, see docker documentation on \`docker run\`'s -v option"
+    echo "    -dp Destination port to map to (default: 8080)"
+    echo "    -h  Display this help dialog"
+    echo "    -i  Interactive mode, overrides default behavior to daemonize"
+    echo "    -s  Boot into interactive shell (implies interactive mode '-i')"
+    echo "    -sp Source port to map (default: 80)"
+    echo "    -t  Specifies tag for briandlee/geoserver image"
+    echo "    -v  Mount volume, see docker documentation on \`docker run\`'s -v option"
     exit ${1}
 }
 
 CONTAINER_HOME=$(cd $(dirname ${0}); pwd | sed 's/ /\\ /')
 
 mounts=("-v ${CONTAINER_HOME}/data:/var/lib/geoserver/data" "-v ${CONTAINER_HOME}/logs:/var/log/tomcat")
-options=('-p 80:8080 -d')
+entrypoint=0
+destport='8080'
 tag='2.7'
+srcport='80'
+runtype='-d'
 args=()
 
 while [[ ${1} ]]; do
@@ -21,10 +28,13 @@ while [[ ${1} ]]; do
         -h)
             help 0
             ;;
-        -i)
-            options=('-it')
+        -s)
+            entrypoint="/bin/bash"
+            runtype='-it'
             mounts+=('-v ~/.bash_history:/root/.bash_history')
-            args=('/bin/bash')
+            ;;
+        -i)
+            runtype='-it'
             ;;
         -t)
             shift
@@ -34,8 +44,16 @@ while [[ ${1} ]]; do
             shift
             mounts+=("-v ${1// /\\ }")
             ;;
+        -dp)
+            shift
+            destport=${1}
+            ;;
+        -sp)
+            shift
+            srcport=${1}
+            ;;
         -*)
-            echo >2 'Unkown option provided: ${1}'
+            echo >&2 'Unkown option provided: ${1}'
             exit 1
             ;;
         *)
@@ -46,6 +64,15 @@ done
 
 pushd ${DOCKER_BUILD_LOCATION} 2>/dev/null
 
-eval "docker run ${mounts[@]} ${options[@]} briandlee/geoserver@${tag} ${args}"
+entrypoint_opt=""
+
+if [[ ${entrypoint} != 0 ]]; then
+    entrypoint_opt="--entrypoint=${entrypoint}"
+fi
+
+cmd="docker run ${entrypoint_opt} -p ${srcport}:${destport} ${runtype} briandlee/geoserver@${tag} ${args[@]}"
+
+echo "${cmd}"
+eval "${cmd}"
 
 popd 2>/dev/null
